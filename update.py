2b317efaab534f28a67adc10658aab79ec8ad225
Juha Autero <jautero@iki.fi>
2020-04-20 07:21:10 +0300

    Added update_docs

--- update.py
+++ update.py
@@ -1,11 +1,18 @@
-import git
-import covid19
+import git, glob, covid19
 
 def update_data(repo):
     cmd=repo.git()
     cmd.pull("--rebase",'upstream','master')
     cmd.push()
 
+def update_docs(repo):
+    covid19.create_topchart_files("docs")
+    repo.add(glob.glob("docs/*.png"))
+    repo.git.commit(m="Updated images in docs/")
+    repo.git.push()
+
+
 if __name__ == '__main__':
     repo=git.Repo(".")
     update_data(repo)
+    update_docs(repo)
